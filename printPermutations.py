fb6373041b6783d7c23a98aa068e572f5a4025cc
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-07-25 18:24:49 +0600

    Create printPermutations.py

---
+++ printPermutations.py
@@ -0,0 +1,15 @@
+def swap(S, i, j):
+    S[i], S[j] = S[j], S[i]
+def permutation(S, left, right):
+    if left == right:
+        print(S)
+        return
+    for i in range(left, right + 1):
+        # do
+        swap(S, left, i)
+        # recurse
+        permutation(S, left + 1, right)
+        # undo
+        swap(S, left, i)
+
+permutation(['a', 'b', 'c'], 0, 2)
