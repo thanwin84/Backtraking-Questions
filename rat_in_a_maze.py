0483397ace63f6f859a0d9d8edf1c0d7aff62b65
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-01 23:46:13 +0600

    Create rat_in_a_maze.py

---
+++ rat_in_a_maze.py
@@ -0,0 +1,34 @@
+//time: O(4^(n*m)) and space: O(n*m)
+class Solution:
+    def __init__ (self):
+        self.result = []
+    def findPath(self, m, n):
+        visited = [[0 for i in range(n)]for j in range(n)]
+        if m[0][0] == 0:
+            return self.result
+        self.solve(m, n, visited, 0, 0, "")
+        return self.result
+    def solve(self, m, n, visited, row, col, move):
+        if row == n -1 and col == n -1:
+            self.result.append(move)
+            return
+        # downward
+        if row + 1 < n and visited[row + 1][col] != 1 and m[row + 1][col] == 1:
+            visited[row][col] = 1
+            self.solve(m, n, visited, row + 1, col, move + 'D')
+            visited[row][col] = 0
+        #left
+        if col - 1 >= 0 and visited[row][col -1] != 1 and m[row][col -1] == 1:
+            visited[row][col] = 1
+            self.solve(m, n, visited, row, col - 1, move + 'L')
+            visited[row][col] = 0
+        # right
+        if col + 1 < n and visited[row][col + 1] != 1 and m[row][col + 1] == 1:
+            visited[row][col] = 1
+            self.solve(m, n, visited, row, col + 1, move + 'R')
+            visited[row][col] = 0
+        # upward
+        if row - 1 >= 0 and visited[row - 1][col] != 1 and m[row - 1][col] == 1:
+            visited[row][col] = 1
+            self.solve(m, n, visited, row - 1, col, move + 'U')
+            visited[row][col] = 0
