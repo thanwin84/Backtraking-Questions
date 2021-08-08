818814b1cc70d7908973d3e3202e912ed2cddb69
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-08 21:06:03 +0600

    Update remove invalid parenthesis.py

--- remove invalid parenthesis.py
+++ remove invalid parenthesis.py
@@ -5,22 +5,28 @@ class Solution:
         self.dp = {}
     def helper(self, s, allowed):
         self.dp[s] = 1
+        # if string is valid parenthesis, we don't need to check further
         if self.is_valid(s):
             # to check duplicate
             if s not in self.table:
                 self.output.append(s)
                 self.table[s] = 1
             return
+        # it makes sure removing minimum number of parenthesis
         if allowed == 0:
             return
 
         for k in range(0, len(s)):
             if s[k] not in ['(', ')']:
                 continue
+             # removing one parenthesis every time
+            # and checking if rest string can form valid parenthesis
             left = s[:k]
             right = s[k + 1:]
+            # avoid solving same problem againa and again
             if left + right in self.dp:
                 continue
+            # allowed becomes allowed -1 cause we just removed one parenthesis
             self.helper(left + right, allowed - 1)
 
 
