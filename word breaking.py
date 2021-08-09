57a3b9ccb674f806f2138dd1bc89fbfa8bc94f6b
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-09 19:56:58 +0600

    Update word breaking.py

--- word breaking.py
+++ word breaking.py
@@ -1,23 +1,22 @@
 # word break using backtracking
+# time: O(2^n)
 class Solution:
     def __init__(self):
         self.output = []
+        self.dp = {}
     def helper(self, start, end,  n, result, table, s):
         if end == n:
-            self.output.append(result)
-            return
+            self.output.append(result[:len(result)-1])
+            return result
 
         for index in range(start, n+1):
+            # partion only when the string is valid
             if s[start:index] in table:
                 self.helper(index, index,  n, result + s[start: index] + ' ',table,  s)
-        return False
-    def wordBreak(self, n, dict, s):
+        return 
+    def wordBreak(self, s: str, wordDict: List[str]) ->List[str]:
         table = {}
-        for word in dict:
+        for word in wordDict:
             table[word] = 1
-        self.helper(0, 0,  n, "", table, s)
+        self.helper(0, 0,  len(s), "", table, s)
         return self.output
-s = "catsanddog"
-di = ["cats", "cat", "and", "sand", "dog"]
-obj = Solution()
-print(obj.wordBreak(len(s), di, s))
