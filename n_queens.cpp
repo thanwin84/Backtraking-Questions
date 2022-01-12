8e7b789c124f18d4384994022aca52329292bbcf
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-01-12 16:12:28 +0600

    Update n_queens.cpp

--- n_queens.cpp
+++ n_queens.cpp
@@ -1,54 +1,58 @@
 //time: O(2^n)
 class Solution {
 public:
-	vector<vector<string>> result;
-	bool isSafe(vector<string> &board, int row, int col, int n) {
-		int r = row;
-		int c = col;
-		while (0 <= r) {
-			if (board[r][c] == 'Q') {
-				return false;
-			}
-			r--;
-		}
-		r = row;
-		c = col;
-		while (r >= 0 and c >= 0) {
-			if (board[r][c] == 'Q') {
-				return false;
-			}
-			r--;
-			c--;
-		}
-		r = row;
-		c = col;
-		while (r >= 0 and c < n) {
-			if (board[r][c] == 'Q') {
-				return false;
-			}
-			r--;
-			c++;
-		}
-		return true;
-	}
-	bool solve(vector<string> &board, int x,  int qpsf, int totalQ) {
-		if (totalQ == qpsf) {
-			result.push_back(board);
-			return true;
-		}
-		for (int col = 0; col < totalQ; col++) {
-			if (isSafe(board, x, col, totalQ)) {
-				board[x][col] = 'Q';
-				solve(board, x + 1, qpsf + 1, totalQ);
-				board[x][col] = '.';
-			}
-		}
-		return false;
-	}
-	vector<vector<string>> solveNQueens(int n) {
-		vector<string> board(n, string(n, '.'));
-		solve(board, 0, 0, n);
-		return result;
-
-	}
+    vector<vector<string>> output;
+    bool is_safe(vector<string> &board, int row, int col) {
+        int r = row;
+        int c = col;
+        //upward left diagonal
+        while (r >= 0 and c >= 0) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+            r--;
+            c--;
+        }
+        //upward right diagonal
+        r = row;
+        c = col;
+        while (r >= 0 and c < board.size()) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+            r--;
+            c++;
+        }
+        r = row;
+        c = col;
+        while (r >= 0) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+            r--;
+        }
+        return true;
+    }
+    bool solve(vector<string>& board, int row, int current_q, int total_q) {
+        if (current_q == total_q) {
+            output.push_back(board);
+            return true;
+        }
+        for (int col = 0; col < total_q; col++) {
+            if (is_safe(board, row, col)) {
+                //do
+                board[row][col] = 'Q';
+                //recurse
+                solve(board, row + 1, current_q + 1, total_q);
+                //undo
+                board[row][col] = '.';
+            }
+        }
+        return false;
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        solve(board, 0, 0, n);
+        return output;
+    }
 };
