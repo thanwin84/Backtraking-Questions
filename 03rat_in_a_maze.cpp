cc0660999c22b75b7bef3f4a8a5ce6a22c7d153e
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-01-12 23:57:13 +0600

    Update 03rat_in_a_maze.cpp

--- 03rat_in_a_maze.cpp
+++ 03rat_in_a_maze.cpp
@@ -43,3 +43,38 @@ public:
         return output;
     }
 };
+//shortcode
+class Solution {
+public:
+    vector<string> output;
+    vector<int> row_list;
+    vector<int> col_list;
+    bool solve(vector<vector<int>>& m, vector<vector<int>>& visited, int n, int row, int col, string move) {
+        if (row == n - 1 and col == n - 1) {
+            output.push_back(move);
+            return true;
+        }
+        string ways = "DLRU";
+        for (int i = 0; i < 4; i++) {
+            int next_row = row + row_list[i];
+            int next_col = col + col_list[i];
+            if (next_row >= 0 and next_col >= 0 and next_row < n and next_col < n and visited[next_row][next_col] != 1
+                and m[next_row][next_col] == 1) {
+                visited[row][col] = 1;
+                solve(m, visited, n, next_row, next_col, move + ways[i]);
+                visited[row][col] = 0;
+            }
+        }
+    }
+    vector<string> findPath(vector<vector<int>>& m, int n) {
+        // Your code goes here
+        vector<vector<int>> visited(n, vector<int>(n, 0));
+        if (m[0][0] == 0){
+            return output;
+        }
+        row_list = { 1, 0, 0, -1 };
+        col_list = { 0, -1, 1, 0 };
+        solve(m, visited, n, 0, 0, "");
+        return output;
+    }
+};
