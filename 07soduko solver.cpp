ed0665d9ec2f44645b0b227ee44d152989bc91e1
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-03-12 23:11:20 +0600

    Update 07soduko solver.cpp

--- 07soduko solver.cpp
+++ 07soduko solver.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
-    bool is_valid(vector<vector<char>>& board, int row, int col, char c) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == c) {
-                return false;
-            }
-            if (board[i][col] == c) {
-                return false;
-            }
-            if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == c) {
-                return false;
-            }
+    bool isValid(vector<vector<char>>& board, int row, int col, char ch){
+        for (int i = 0; i < 9; i++){
+            // check entire col
+            if (board[row][i] == ch) return false;
+            // check entire row
+            if (board[i][col] == ch) return false;
+            // in each 3 * 3 matrix
+            int r = 3 * (row / 3) + (i / 3);
+            int c = 3 * (col / 3) + (i % 3);
+            if (board[r][c] == ch) return false;
         }
         return true;
     }
-    bool solve(vector<vector<char>>& board, int row, int col) {
-        if (row == 8 and col == 9) {
+    bool solve(vector<vector<char>>& board, int row, int col){
+        // we've got a solution
+        if (row == 8 && col == 9){
             return true;
         }
-        if (col == 9) {
-            row += 1;
-            col = 0;
+        // go to next row
+        if (col == 9){
+            row++;
+            col = 0; 
         }
-        if (board[row][col] == '.') {
-            for (char c = '1'; c <= '9'; c++) {
-                if (is_valid(board, row, col, c)) {
-                    board[row][col] = c;
-                    if (solve(board, row, col + 1)) {
+        if (board[row][col] == '.'){
+            for (char ch = '1'; ch <= '9'; ch++){
+                if(isValid(board, row, col, ch)){
+                    // do
+                    board[row][col] = ch;
+                    if (solve(board, row, col + 1)){
                         return true;
                     }
-                    else {
-                        board[row][col] = '.';
-                    }
+                    // undo
+                    board[row][col] = '.';
                 }
             }
             return false;
         }
+        // go to next col
         else {
             return solve(board, row, col + 1);
         }
