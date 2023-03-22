a1bd3f0787e9c025e76f7b535c3fa0e0078f25d9
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-03-23 01:06:23 +0600

    Update 05find all palindrones in a string.cpp

--- 05find all palindrones in a string.cpp
+++ 05find all palindrones in a string.cpp
@@ -1,36 +1,35 @@
 //time: O(N * 2^n)
 class Solution {
-public:
     vector<vector<string>> output;
-    vector<string> palindrones;
-    bool is_palindrone(string s) {
-        int i = 0;
-        int j = s.size() - 1;
-        while (i <= j) {
-            if (s[i] != s[j]) {
+    vector<string> temp;
+public:
+    bool isPalindrone(string &s, int start, int end){
+        while(start <= end){
+            if (s[start] != s[end]){
                 return false;
             }
-            i++;
-            j--;
+            start++;
+            end--;
         }
         return true;
     }
-    void solve(string s) {
-        if (s.size() == 0) {
-            output.push_back(palindrones);
+    void solve(string &s, int start){
+        if (start == s.size()){
+            output.push_back(temp);
             return;
         }
-        for (int k = 0; k < s.size(); k++) {
-            if (is_palindrone(s.substr(0, k + 1))) {
-                palindrones.push_back(s.substr(0, k + 1));
-                solve(s.substr(k + 1));
-                palindrones.pop_back();
+        for (int end = start; end < s.size(); end++){
+            // partition only if the current substring is a palindrone
+            if (isPalindrone(s, start, end)){
+                temp.push_back(s.substr(start, end - start + 1));
+                // recurse for the rest of the substring
+                solve(s, end + 1);
+                temp.pop_back();
             }
         }
     }
-    vector<vector<string>> allPalindromicPerms(string S) {
-        // code here
-        solve(S);
+    vector<vector<string>> partition(string s) {
+        solve(s, 0);
         return output;
     }
 };
