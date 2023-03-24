073933e5aeb3310911f8865a3ff09e81b7a6c799
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-03-24 17:30:17 +0600

    Update combination sum.cpp

--- combination sum.cpp
+++ combination sum.cpp
@@ -1,35 +1,29 @@
 
 class Solution {
+    vector<vector<int>> combinations;
+    vector<int> combination;
 public:
-    
-    vector<vector<int>> comb_li;
-    vector<int> temp_arr;
-    unordered_map<string, int> seen;
-    void solve(vector<int>& nums, int target, int ind, string arr) {
-        if (target == 0) {
-            if (!seen.count(arr)) {
-                seen[arr] = 1;
-                comb_li.push_back(temp_arr);
-            }
+    void solve(vector<int> &candidates, int target, int currentSum, int choice){
+        if (choice == candidates.size() || currentSum > target){
             return;
         }
-        if (ind < 0) {
+        if (currentSum == target){
+            combinations.push_back(combination);
             return;
         }
-        if (nums[ind] <= target) {
-            //do
-            temp_arr.push_back(nums[ind]);
-            //recurse
-            solve(nums, target - nums[ind], ind, arr + to_string(nums[ind])+'-');
-            //undo
-            temp_arr.pop_back();
-        }
-        solve(nums, target, ind - 1, arr);
+        currentSum += candidates[choice];
+        combination.push_back(candidates[choice]);
+        solve(candidates, target, currentSum, choice);
+        currentSum -= candidates[choice];
+        combination.pop_back();
+        solve(candidates, target, currentSum, choice + 1);
     }
-    vector<vector<int> > combinationSum(vector<int>& A, int target) {
-        // Your code here
-        sort(A.begin(), A.end(), greater<int>());
-        solve(A, target, A.size() -1, "");
-        return comb_li;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        solve(candidates, target, 0, 0);
+        return combinations;
     }
 };
+
+// time complexity: O(2^n) * K(average size of combination)
+// space: since the size of combination is unpredictable, let's assume the size of the combinations is k and size 
+// of the combination is x, so time complexity will be O(k * x)
